ef992d6636e8b53be4e2307494a972b553c4e6f1
Zeetito <agyareernest44@gmail.com>
2024-10-09 12:49:45 +0000

    OnBoardingScreen - Video 6 complete

---
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_next_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/controllers.onboarding/onboarding.controller.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/devices/device_utility.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+        onPressed: () {
+          OnBoardingController.instance.nextPage();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: dark ? TColors.primary : Colors.black,
+          shape: const CircleBorder(),
+        ),
+        child: const Icon(Iconsax.arrow_right_3),
+      ),
+    );
+  }
+}
